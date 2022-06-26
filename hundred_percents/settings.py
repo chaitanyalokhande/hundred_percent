ce0d29689e6bab9c58ab9f44b7251a0e8dedf5bf
Chaitanya Lokhande <92268163+chaitanyalokhande@users.noreply.github.com>
2022-06-26 14:10:48 -0400

    Update settings.py

--- hundred_percents/settings.py
+++ hundred_percents/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-us^!1165wh(#!7j74_h=_du229fy$4*8&6mq3n+(x8rsi8ve0j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','hundred100percent.herokuapp.com']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
